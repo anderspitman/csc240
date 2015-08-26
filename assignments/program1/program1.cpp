4ca49e2277dd276fc31226d18e7f807b9d69f46b
Anders Pitman <tapitman11@gmail.com>
2015-08-26 14:23:20 -0700

    Initial commit. Program 1

---
+++ assignments/program1/program1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Name:" << endl;
+    cout << "  Anders Pitman" << endl;
+
+    cout << "Prior CS Coursework:" << endl;
+    cout << "  CSC110 (Zerange)" << endl;
+    cout << "  CSC205 (Heil)" << endl;
+    cout << "  Bioinformatics Algorithms 1 (Coursera MOOC)" << endl;
+    cout << "  Functional Programming Prin. in Scala (Coursera MOOC)" << endl;
+
+    cout << "Professional Background:" << endl;
+    cout << "  6 years software engineering experience. 2 in" << endl;
+    cout << "  avionics, 3 in telecom, and 1 in bioinformatics" << endl;
+
+    cout << "Other computer experience:" << endl;
+    cout << "  Sometimes do PC builds." << endl;
+
+    cout << "Programming Environment:" << endl;
+    cout << "  Linux + VIM + Makefiles + gcc." << endl;
+
+    cout << "Languages:" << endl;
+    cout << "  C, C++, Python, JavaScript, Scala, Rust, Go, HTML, CSS" << endl;
+    cout << "  Ada, Scheme. Probably forgetting 1 or 2." << endl;
+
+    cout << "Goals for course:" << endl;
+    cout << "  Fulfill prereq and have fun learning! Especially" << endl;
+    cout << "  looking foward to digging into Lisp some more." << endl;
+
+    cout << "Concerns:" << endl;
+    cout << "  Nada." << endl;
+
+    cout << "Other classes:" << endl;
+    cout << "  ECE102 and ECE103." << endl;
+
+    cout << "Favoriate Program I've Written:" << endl;
+    cout << "  At my last job I made a plotting framework" << endl;
+    cout << "  in C and Python that enabled us to visualize wireless" << endl;
+    cout << "  channels in real time. It was pretty cool." << endl;
+
+    cout << "Personal Interest:" << endl;
+    cout << "  I love hiking, reading, playing fiddle and guitar." << endl;
+
+    return 0;
+}
