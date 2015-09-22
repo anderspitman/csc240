de960d947d7c7661711e5729cad3ae724254021e
Anders Pitman <tapitman11@gmail.com>
2015-09-21 23:17:43 -0700

    Update test target in Makefile

---
+++ assignments/program4/test/test_program4.cpp
@@ -0,0 +1,5 @@
+#include "catch.hpp"
+
+TEST_CASE( "Dummy test", "[tags?]" ) {
+    REQUIRE( true == true );
+}
--- assignments/program4/test/tester.cpp
+++ assignments/program4/test/tester.cpp
@@ -1,9 +1,2 @@
-#include <iostream>
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
-using namespace std;
-
-int main(int argc, char **argv) {
-    cout << "Hi there" << endl;
-    return 0;
-}
