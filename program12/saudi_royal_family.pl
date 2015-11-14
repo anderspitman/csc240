9a75aab0811c823504426b2e8253352afd228972
Anders Pitman <tapitman11@gmail.com>
2015-11-13 18:45:24 -0700

    Shorten names and add basic rules
    For program 12

---
+++ program12/saudi_royal_family.pl
@@ -0,0 +1,107 @@
+% Anders Pitman - Program 10 - Family Tree for Saudi Arabian Royal Family
+
+% male( <name> ).
+male(turki ).
+male(faisal ).
+male(abdul_rahman ).
+male(saud ).
+male(abdulaziz ).
+male(saud_bin_abdulaziz ).
+male(faisal_bin_abdulaziz ).
+male(khalid ).
+male(fahd ).
+male(abdullah ).
+male(salman ).
+
+% female( <name> ).
+female(wadhah ).
+female(tarfa ).
+female(al_jawhara ).
+female(hassa ).
+female(fahda ).
+
+% lifespan( <name>, <birth year>, <death year>).
+lifespan(wadhah, unknown, unknown).
+lifespan(tarfa, unknown, unknown).
+lifespan(turki, 1755, 1834 ).
+lifespan(faisal, 1785, 1865 ).
+lifespan(saud, unknown, 1875 ).
+lifespan(abdul_rahman, 1845, 1928 ).
+lifespan(abdulaziz, 1875, 1953 ).
+lifespan(saud_bin_abdulaziz, 1902, 1969 ).
+lifespan(faisal_bin_abdulaziz, 1906, 1975 ).
+lifespan(khalid, 1913, 1982 ).
+lifespan(fahd, 1921, 2005 ).
+lifespan(abdullah, 1924, 2015 ).
+lifespan(salman, 1935, unknown).
+lifespan(al_jawhara, unknown, 1919 ).
+lifespan(hassa, 1900, 1969 ).
+lifespan(fahda, unknown, 1930 ).
+
+% parentOf( <name>, <child name>).
+parentOf(turki, faisal ).
+parentOf(faisal, abdul_rahman).
+parentOf(faisal, saud ).
+parentOf(abdul_rahman, abdulaziz).
+parentOf(abdulaziz, saud_bin_abdulaziz ).
+parentOf(abdulaziz, faisal_bin_abdulaziz).
+parentOf(abdulaziz, khalid).
+parentOf(abdulaziz, fahd).
+parentOf(abdulaziz, abdullah).
+parentOf(abdulaziz, salman).
+parentOf(wadhah, saud_bin_abdulaziz ).
+parentOf(tarfa, faisal_bin_abdulaziz).
+parentOf(al_jawhara, khalid).
+parentOf(hassa, fahd).
+parentOf(fahda, abdullah).
+parentOf(hassa, salman).
+
+% rulerOf( <name>, <country>, <year began>, <year ended> ).
+rulerOf(turki, second_saudi_state, 1818, 1834 ).
+rulerOf(faisal, second_saudi_state, 1834, 1838 ).
+rulerOf(faisal, second_saudi_state, 1843, 1865 ).
+rulerOf(saud, second_saudi_state, 1871, 1871 ).
+rulerOf(saud, second_saudi_state, 1873, 1875 ).
+rulerOf(abdul_rahman, second_saudi_state, 1875, 1876 ).
+rulerOf(abdul_rahman, second_saudi_state, 1889, 1891 ).
+rulerOf(abdulaziz, saudi_arabia, 1932, 1953 ).
+rulerOf(saud_bin_abdulaziz, saudi_arabia, 1953, 1964 ).
+rulerOf(faisal_bin_abdulaziz, saudi_arabia, 1964, 1975 ).
+rulerOf(khalid, saudi_arabia, 1975, 1982 ).
+rulerOf(fahd, saudi_arabia, 1982, 2005 ).
+rulerOf(abdullah, saudi_arabia, 2005, 2015 ).
+rulerOf(salman, saudi_arabia, 2015, current ).
+
+motherOf(Mother, Child)
+    :- female(Mother),
+       parentOf(Mother, Child).
+
+fatherOf(Father, Child)
+    :- male(Father),
+       parentOf(Father, Child).
+
+sonOf(Son, Parent)
+    :- male(Son),
+       parentOf(Parent, Son).
+
+daughterOf(Daughter, Parent)
+    :- female(Daughter),
+       parentOf(Parent, Daughter).
+
+% For the sibling definitions below, I chose to include half siblings since
+% the kings often have children from multiple wives.
+
+brotherOf(Brother, Sibling)
+    :- male(Brother),
+       (motherOf(Mother, Brother), motherOf(Mother, Sibling);
+        fatherOf(Father, Brother), fatherOf(Father, Sibling)).
+
+sisterOf(Sister, Sibling)
+    :- female(Sister),
+       (motherOf(Mother, Sister), motherOf(Mother, Sibling);
+        fatherOf(Father, Sister), fatherOf(Father, Sibling)).
+
+siblingOf(Person, Sibling)
+    :- (motherOf(Mother, Person), motherOf(Mother, Sibling);
+        fatherOf(Father, Person), fatherOf(Father, Sibling)).
+       
